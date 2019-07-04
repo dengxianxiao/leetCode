2b232d09b82dce5004dbfe0a25ea2837101f1e94
dengxianxiao <dengxianxiao@gmail.com>
2019-07-04 08:48:03 +0800

    167. 两数之和 II - 输入有序数组

---
+++ easy/_167/_167.js
@@ -0,0 +1,67 @@
+/**
+ * 167. 两数之和 II - 输入有序数组
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+说明:
+
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+示例:
+
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+
+ */
+
+
+ /**
+  * 双指针，定义两个头尾指针i, j，如何求和大于target，则j-- ,小于则i++
+  *
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum1 = function(numbers, target) {
+  if (!numbers || !numbers.length) {
+      return []
+  }
+  let i = 0
+  let j = numbers.length - 1
+  while (i < j) {
+      let sum = numbers[i] + numbers[j]
+      if (target === sum) {
+          return [i + 1, j + 1]
+      } else if (sum > target) {
+          j--
+      } else {
+          i++
+      }
+  }
+  return []
+};
+
+/**
+ * 双层循环
+ *
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum2 = function(numbers, target) {
+  if (!numbers || !numbers.length) {
+      return []
+  }
+  let len = numbers.length
+  for (let i = 0; i < len; i++) {
+      let remain = target - numbers[i]
+      for (let j = i + 1; j < len; j++) {
+          if (remain === numbers[j]) {
+              return [i + 1, j + 1]
+          }
+      }
+  }
+  return []
+};
