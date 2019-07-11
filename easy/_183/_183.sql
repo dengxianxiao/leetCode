b9759e47292779d44f6ad85d1936a4f020b242f0
dengxianxiao <dengxianxiao@gmail.com>
2019-07-11 09:23:36 +0800

    183. 从不订购的客户

---
+++ easy/_183/_183.sql
@@ -0,0 +1,36 @@
+某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+
+Customers 表：
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders 表：
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+例如给定上述表格，你的查询应返回：
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+# Write your MySQL query statement below
+
+select Name As Customers from Customers where id not in (select CustomerId from Orders)
+
+
+select a.Name As Customers from Customers a left join Orders b on a.Id = b.CustomerId where b.CustomerId is null
