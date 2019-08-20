e99b05f88f895e6cf2f00680530466ac0eba14c5
dengxianxiao <dengxianxiao@gmail.com>
2019-08-20 09:02:57 +0800

    283. 移动零

---
+++ easy1/_283/_283.js
@@ -0,0 +1,33 @@
+/**
+ * 283. 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let len = nums.length
+  let i = 0
+  while (i < len) {
+      if (nums[i] === 0) {
+          let currValue = nums.splice(i, 1)
+          nums.push(currValue)
+          len--
+      } else {
+          i++
+      }
+  }
+  return nums
+};
