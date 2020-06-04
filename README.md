3c28dcf25e7ceffd6eee88a68a1373b225b66046
dengxianxiao <dengxianxiao@gmail.com>
2020-06-04 09:40:52 +0800

    543. 二叉树的直径

--- README.md
+++ README.md
@@ -137,6 +137,7 @@ LeetCode 算法练习
 - [No.532 数组中的K-diff数对](./easy1/_532/_532.js)
 - [No.538 把二叉搜索树转换为累加树](./easy1/_538/_538.js)
 - [No.541 反转字符串 II](./easy1/_541/_541.js)
+- [No.543 二叉树的直径](./easy1/_543/_543.js)
 - [No.557 Reverse Words in a String](./easy/_557/_557.js)
 - [No.617 Merge Two Binary Trees](./easy/_617/_617.js)
 - [No.657 Judge Route Circle](./easy/_657/_657.js)
---
+++ easy1/_543/_543.js
@@ -0,0 +1,48 @@
+/**
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+ 
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \
+      4   5
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+ 
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+let ans = 0
+var diameterOfBinaryTree = function(root) {
+    ans = 1
+    depth(root)
+    return ans - 1
+};
+
+var depth = function(node) {
+    if (!node) {
+        return 0
+    }
+    const left = depth(node.left)
+    const right = depth(node.right)
+    ans = Math.max(ans, left + right + 1)
+    return Math.max(left, right) + 1
+}
